8205c737898e0b1ccdd9adee83a76d4e79f08656
Ckrenzer <ckrenzer.info@gmail.com>
2025-10-11 23:30:57 -0400

    setting slurp and barf keybindings

--- ftplugin/lisp/paredit.lua
+++ ftplugin/lisp/paredit.lua
@@ -1,2 +1,9 @@
 local paredit = require("nvim-paredit")
-paredit.setup()
+paredit.setup({
+    keys = {
+        [">)"] = { paredit.api.slurp_forwards, "Slurp forwards" },
+        [">("] = { paredit.api.slurp_backwards, "Slurp backwards" },
+        ["<)"] = { paredit.api.barf_forwards, "Barf forwards" },
+        ["<("] = { paredit.api.barf_backwards, "Barf backwards" },
+    }
+})
