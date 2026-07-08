7132a9e6daa16b601993ec6dd1673d177bfdeae0
Ckrenzer <ckrenzer.info@gmail.com>
2026-07-07 23:38:31 -0400

    updated language servers script to reflect what is actually used

--- servers.sh
+++ servers.sh
@@ -1,23 +1,25 @@
 # you'll have to ensure that the language servers
 # are installed for the LSPs to work.
+#
+# see `:help lspconfig-all` for the full supported list
 
-sudo npm -i -g awk-language-server
-sudo npm -i -g bashls
-sudo npm -i -g pyright
-# julia -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer")'
-npm i -g sql-language-server
+# AWK
+sudo npm install -g awk-language-server
+
+# BASH
+sudo npm install -g bashls
+
+# Lua
+sudo pacman -S lua-language-server
+
+# Python
+pipx install basedpyright
+
+# R
+R -e 'install.packages("languageserver")'
+
+# SQL
+sudo npm install -g sql-language-server
+
+# Vimscript
 sudo npm install -g vim-language-server
-
-R -e "install.packages('languageserver')" # R needs this package to be installed
-
-# need to do some manual work for lua:
-# need dependency ninja
-# need C++17 compiler
-start="$PWD"
-sudo apt install ninja-build
-git clone https://github.com/LuaLS/lua-language-server "$HOME/repos/forks_and_clones/"
-cd "$HOME/repos/forks_and_clones/lua-language-server"
-./make.sh
-cd "$start"
-# you'll need to point your computer to the binary created by the above install
-# (add the installation location to $PATH)
